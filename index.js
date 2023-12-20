d7bc2efb3ca7bafe3acd0f2693c272d5253ae05a
Benjamin Martinez <benjamin.martinez@ti.com>
2023-12-20 14:20:09 -0600

    STEP-01-SETUP-ENVYRONMENT

---
+++ index.js
@@ -0,0 +1,25 @@
+const {    
+    AccountId,
+    PrivateKey,        
+  } = require("@hashgraph/sdk");
+  
+  require("dotenv").config();
+  
+  async function environmentSetup() {
+    // Part 1 - Envyronment variables
+
+    //Grab your Hedera testnet account ID and private key from your .env file
+    //Construct accountId & privateKey from String
+    const myAccountId = AccountId.fromString(process.env.MY_ACCOUNT_ID);
+    const myPrivateKey = PrivateKey.fromString(process.env.MY_PRIVATE_KEY);
+  
+    // If we weren't able to grab it, we should throw a new error
+    if (!myAccountId || !myPrivateKey) {
+      throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY are not present!");
+    }else{
+        console.log(`myAccountId = ${myAccountId}`);
+        console.log(`myPrivateKey = ${myPrivateKey}`);                
+    }
+
+  }
+  environmentSetup();
