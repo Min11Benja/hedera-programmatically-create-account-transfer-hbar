c4d523b295c40d7f17d4c9cc4e6f0ad3c4115de7
min11benja <min11benja@hotmail.com>
2024-01-03 18:26:28 -0600

    STEP-3-ADD-ACCOUNT

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-const { AccountId, PrivateKey, Client, Hbar } = require("@hashgraph/sdk");
-
+const {AccountId,Client,PrivateKey,AccountCreateTransaction,AccountBalanceQuery,Hbar,} = require("@hashgraph/sdk");
 require("dotenv").config();
-
+  //STEP 1 import already made account
 async function environmentSetup() {
-// Part 1 - Envyronment variables
-
-  //Grab your Hedera testnet account ID and private key from your .env file
-  //Construct accountId & privateKey from String
   const myAccountId = AccountId.fromString(process.env.MY_ACCOUNT_ID);
   const myPrivateKey = PrivateKey.fromString(process.env.MY_PRIVATE_KEY);
-
-  // If we weren't able to grab it, we should throw a new error
-  if (!myAccountId || !myPrivateKey) {
-    throw new Error(
-      "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY are not present!"
-    );
-  } else {
+  if (!myAccountId || !myPrivateKey){
+    throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY are not present!");
+  }else {
     console.log(`myAccountId = ${myAccountId}`);
     console.log(`myPrivateKey = ${myPrivateKey}`);
   }
-// Part 2 - Create connection to the Hedera network
+  //STEP 2 Connect to Testnet
   const client = Client.forTestnet();
   client.setOperator(myAccountId, myPrivateKey);
-
-  //Set your account as the client's operator
-  client.setOperator(myAccountId, myPrivateKey);
-
-  //Set the default maximum transaction fee (in Hbar)
   client.setDefaultMaxTransactionFee(new Hbar(100));
-
-  //Set the maximum payment for queries (in Hbar)
   client.setMaxQueryPayment(new Hbar(50));
-  console.log("Conection made succesfully!");     
-  client.close();  
+  console.log("Conection made succesfully!");
+  //STEP 3 Make New Account  
+  const newAccountPrivateKey = PrivateKey.generateED25519();
+  const newAccountPublicKey = newAccountPrivateKey.publicKey;
+  if (!newAccountPrivateKey || !newAccountPublicKey) {
+    throw new Error("New keys are null");
+  } else {
+    console.log("New Keys made succesfully!");
+    console.log(`newAccountPrivateKey = ${newAccountPrivateKey}`);
+    console.log(`newAccountPublicKey = ${newAccountPublicKey}`);
+  }
+  // Create a new account with 1,000 tinybar starting balance
+  const newAccount = await new AccountCreateTransaction().setKey(newAccountPublicKey).setInitialBalance(Hbar.fromTinybars(1000)).execute(client);
+
+  if (!newAccount) {
+    throw new Error("New account is null");
+  } else {
+    console.log("New account made succesfully!");     
+    console.log(`newAccount = ${newAccount}`); 
+  }
+  // Get the new account ID
+    const getReceipt = await newAccount.getReceipt(client);
+    const newAccountId = getReceipt.accountId;
+    console.log("\nNew account ID: " + newAccountId);
+    
+    // Verify the account balance
+    const accountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+    console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
+  
+    console.log("End");
+    client.close();  
+    return newAccountId;
 }
 environmentSetup();
