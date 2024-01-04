6a41fa55db9d535ccd941305ee2fe7cdf254bc9f
min11benja <min11benja@hotmail.com>
2024-01-03 18:53:21 -0600

    STEP-4-TRANSFER-HBAR

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const {AccountId,Client,PrivateKey,AccountCreateTransaction,AccountBalanceQuery,Hbar,} = require("@hashgraph/sdk");
+const {AccountId,Client,PrivateKey,AccountCreateTransaction,AccountBalanceQuery,Hbar,TransferTransaction,} = require("@hashgraph/sdk");
 require("dotenv").config();
+
+
+
+
   //STEP 1 import already made account
 async function environmentSetup() {
   const myAccountId = AccountId.fromString(process.env.MY_ACCOUNT_ID);
@@ -44,6 +48,13 @@ async function environmentSetup() {
     const accountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
     console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
   
+    //Create the transfer transaction
+    const sendHbar = await new TransferTransaction().addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000)).addHbarTransfer(newAccountId, Hbar.fromTinybars(1000)).execute(client);
+
+    //Verify the transaction reached consensus
+    const transactionReceipt = await sendHbar.getReceipt(client);
+    console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
+
     console.log("End");
     client.close();  
     return newAccountId;
